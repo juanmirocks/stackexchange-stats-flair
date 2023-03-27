22a4249e5ba415c23076d2909ce8dd5903fa9c60
Dr. Juan Miguel Cejuela <i@juanmi.rocks>
2023-03-27 12:12:58 +0200

    minor

--- src/httpServer.ts
+++ src/httpServer.ts
@@ -1,9 +1,9 @@
 import { serve } from "https://deno.land/std@0.181.0/http/server.ts";
 
 const handler = async (_request: Request): Promise<Response> => {
-  const userID = "341320";
+  const userId = "341320";
   const site = "stackoverflow";
-  const url = `https://api.stackexchange.com/2.3/users/${userID}?site=${site}`;
+  const url = `https://api.stackexchange.com/2.3/users/${userId}?site=${site}`;
   const resp = await fetch(url, {
     headers: {
       accept: "application/json",
