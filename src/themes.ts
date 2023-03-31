6dc2f3e4043a748c3e9e29ee107a40cf02621a24
Dr. Juan Miguel Cejuela <i@juanmi.rocks>
2023-03-31 13:01:06 +0200

    use hex-based colors

--- src/themes.ts
+++ src/themes.ts
@@ -1,7 +1,8 @@
 export const DEFAULT_STYLES = {
   "goldColor": "gold",
   "silverColor": "silver",
-  "bronzeColor": "rgb(207,143,92)",
+  // This is a brighter color. Classic flair uses #CC9966, SO (2023-04) uses #D1A684
+  "bronzeColor": "#CF8F5C",
   textFontFamily:
     '-apple-system, BlinkMacSystemFont, "Segoe UI Adjusted", "Segoe UI", "Liberation Sans", sans-serif',
   textFontSize: "13px",
@@ -10,9 +11,15 @@ export const DEFAULT_STYLES = {
 
 export const THEMES: _ThemesMap = {
   "classic-flair-default": {
-    bgColor: "rgb(234,234,234)",
-    borderColor: "rgb(194,194,194)",
-    displayNameColor: "rgb(0,116,204)",
+    bgColor: "#EBEBEB",
+    borderColor: "#C5C5C5",
+    displayNameColor: "#2A6AC4",
+    reputationColor: "#3C3C3C",
+    badgeCountsColor: "#75767A",
+    textFontFamily: DEFAULT_STYLES.textFontFamily,
+    textFontSize: DEFAULT_STYLES.textFontSize,
+    textReputationFontWeight: DEFAULT_STYLES.textReputationFontWeight
+  },
     reputationColor: "rgb(22,22,22)",
     badgeCountsColor: "rgb(121,122,127)",
     textFontFamily: DEFAULT_STYLES.textFontFamily,
